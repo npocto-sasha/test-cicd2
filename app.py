ee0b23f84eb3b1e9a5219cfcd2353c0471735d1d
Александр Николашкин <nikolashkinalex@gmail.com>
2026-02-14 15:43:21 +0900

    asddasasd

---
+++ app.py
@@ -0,0 +1,15 @@
+import gradio as gr
+
+
+def greet(image):
+    return "Hello, world!"
+
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["image"],
+    outputs=["text"],
+    api_name="predict"
+)
+
+demo.launch()
